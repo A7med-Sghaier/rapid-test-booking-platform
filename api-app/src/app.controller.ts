2384f6a487d9a0c82ac40e4ed308ae602a5b1ef4
Ahmed Sghaier <a7mado008@gmail.com>
2026-07-01 18:11:02 +0200

    Add direct settings endpoint fallback

--- api-app/src/app.controller.ts
+++ api-app/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { Db } from 'mongodb';
 import { AppService } from './app.service';
+import { AppConfigService } from './config/app-config/app-config.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly appConfig: AppConfigService,
+    @Inject('MONGO-PROVIDER') private readonly db: Db
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(['admin/settings', 'settings'])
+  async getSettings(): Promise<any[]> {
+    return this.db.collection(this.appConfig.settingsCollection).find().toArray();
   }
 }
